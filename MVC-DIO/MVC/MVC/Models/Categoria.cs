5aad493c963fb7eb9d976187f44b699199c9d11e
rogerch93 <rogerinhochaves@hotmail.com>
2021-08-29 03:45:37 -0300

    Criando Banco de Dados

---
+++ MVC-DIO/MVC/MVC/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        public String Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ MVC-DIO/MVC/MVC/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
